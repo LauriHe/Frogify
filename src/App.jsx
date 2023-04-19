9429a7154bdbdb1262980995e2fa008fdeab0477
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2023-04-19 22:35:54 +0300

    login working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {MediaProvider} from './contexts/MediaContext';
+import './style.scss';
 import Home from './views/Home';
 import Search from './views/Search';
 import Upload from './views/Upload';
 import Profile from './views/Profile';
 import Layout from './views/Layout';
-import {MediaProvider} from './contexts/MediaContext';
+import Login from './views/Login';
 
 const App = () => {
   return (
     <Router basename={import.meta.env.BASE_URL}>
       <MediaProvider>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<Search />} />
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,65 @@
+import {
+  Button,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from '@mui/material';
+import {useState} from 'react';
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
+import {themeOptions} from '../theme/themeOptions';
+
 const Login = () => {
-  return <></>;
+  const theme = createTheme(themeOptions);
+  const [formToggle, setFormToggle] = useState(true);
+  const toggle = () => {
+    setFormToggle(!formToggle);
+  };
+  const setFormSignIn = () => {
+    setFormToggle(true);
+  };
+  const setFormSignUp = () => {
+    setFormToggle(false);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid container direction="column" alignItems="center" mt={5}>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          sx={{width: '75%'}}
+          gap={1}
+        >
+          <Grid item container direction="column" alignItems="center" gap={5}>
+            <Typography component="h1" variant="h1" color="#1ed760">
+              Frogify
+            </Typography>
+            <Grid item>
+              <Button
+                color={formToggle ? 'secondary' : 'primary'}
+                onClick={setFormSignIn}
+              >
+                Sign in
+              </Button>
+              <Button
+                color={formToggle ? 'primary' : 'secondary'}
+                onClick={setFormSignUp}
+              >
+                Sign up
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item>
+            {formToggle ? <LoginForm /> : <RegisterForm toggle={toggle} />}
+          </Grid>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 };
 
 export default Login;
