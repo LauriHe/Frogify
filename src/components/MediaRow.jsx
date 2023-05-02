c7c38581dcc2a697df2d503741727100f4e85d5a
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2023-05-02 21:39:15 +0300

    added more song info to player and home

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -3,21 +3,35 @@ import PropTypes from 'prop-types';
 import {mediaUrl} from '../utils/variables';
 import commentIcon from '../assets/comment.svg';
 import likeIcon from '../assets/like.svg';
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {SongContext} from '../contexts/SongContext';
+import {useUser} from '../hooks/ApiHooks';
+import {MediaContext} from '../contexts/MediaContext';
 
 const MediaRow = ({file, mediaArray}) => {
   const {setCurrentSong, setCurrentSongImage} = useContext(SongContext);
+  const {user} = useContext(MediaContext);
+  const [postMaker, setPostMaker] = useState('');
+  const {getUser} = useUser();
 
   const image = mediaArray.find(
     (item) => item.file_id === JSON.parse(file.description).imageId
   );
+
+  const fetchUser = async () => {
+    const token = localStorage.getItem('userToken');
+    const user = await getUser(file.user_id, token);
+    setPostMaker(user);
+  };
 
   const playAudio = () => {
     setCurrentSong(file);
     setCurrentSongImage(image);
   };
 
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
       <Box sx={{mb: '1rem'}}>
@@ -26,12 +40,15 @@ const MediaRow = ({file, mediaArray}) => {
             src={mediaUrl + image.thumbnails.w640}
             alt={`cover art for ${file.title}`}
           />
-          <ImageListItemBar title={file.title} />
+          <ImageListItemBar
+            title={file.title}
+            subtitle={user ? postMaker.username : ''}
+            actionIcon={<Button onClick={playAudio}>play</Button>}
+          />
         </ImageListItem>
         <Box sx={{display: 'flex', margin: '.5rem', gap: '.5rem'}}>
           <img src={likeIcon} alt="home icon" width="30rem" />
           <img src={commentIcon} alt="home icon" width="30rem" />
-          <Button onClick={playAudio}>play</Button>
         </Box>
       </Box>
     </>
